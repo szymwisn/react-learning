725fcdffc65cdf0cd6baa9233513d6fb7e0606d3
Szymon Wiśniewski <szymwisn@gmail.com>
2020-04-03 22:00:59 +0200

    Components basics

---
+++ 1. Component basics/src/components/Form/Input/Input.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Input.module.scss'
+
+const Input = ({ tag: Tag, name, label, maxLength }) => (
+    <div className={styles.formItem}>
+        <Tag
+            className={Tag === 'textarea' ? styles.textarea : styles.input}
+            type="text"
+            name={name}
+            id={name}
+            maxLength={maxLength}
+            placeholder=' '
+        />
+        <label className={styles.label} htmlFor={name}>
+            {label}
+        </label>
+        <div className={styles.formItemBar} />
+    </div>
+);
+
+Input.propTypes = {
+    tag: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    maxLength: PropTypes.number
+}
+
+Input.defaultProps = {
+    tag: 'input',
+    maxLength: 30
+}
+
+export default Input;
---
+++ 1. Component basics/src/components/Title/Title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Title.module.scss';
+
+const Title = ({ children }) => (
+    <h2 className={styles.name}>{children}</h2>
+);
+
+export default Title;
