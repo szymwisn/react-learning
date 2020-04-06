f5ab49fe5c2dff84ff2d8eefacd9c38aba237667
Szymon Wiśniewski <szymwisn@gmail.com>
2020-04-06 14:12:49 +0200

    Added styled components

---
+++ favnotes/src/components/Button/Button.js
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+  background-color: #ffd82b;
+  width: ${({ width }) => width || "220px"};
+  height: 47px;
+  border: 0;
+  border-radius: 50px;
+  font-weight: 500;
+  font-size: 16px;
+  text-transform: uppercase;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: #e6e6e6;
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+    `}
+`;
+
+export default Button;
---
+++ favnotes/src/theme/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap');
+
+    *, *::before, *::after {
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    html {
+        font-size: 62.5%; // 1rem === 10px
+    }
+
+    body {
+        font-size: 1.6rem; // bring back font-size and still have 1 rem as 10px, so called "happy rems"
+        font-family: "Montserrat", sans-serif;
+    }
+`;
+
+export default GlobalStyle;
